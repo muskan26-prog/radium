eacd833a7e187d5334d4ad59baa82afc0b301e45
Muskan Sayyad <muskan26sayyad@gmail.com>
2021-11-15 21:47:16 +0530

    mongoDb basic-folder structure and models for interacting with data

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
 const router = express.Router();
+const UserModel= require("../models/userModel")
+const BookModel= require("../models/bookModel")
+
+const UserController= require("../controllers/userController")
+const BookController = require("../controllers/bookController")
+
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+
+router.post('/createUser',  UserController.createUser  );
+router.get('/getAllUsers',  UserController.getUsersData  );
+
+router.post('/createBook', BookController.createBook );
+router.get('/getAllBooks', BookController.getBooksData);
+
 
 module.exports = router;
